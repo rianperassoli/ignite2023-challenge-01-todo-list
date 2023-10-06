adf55310d7ea04f295bfaac024c265b2fa75a16b
Rian Perassoli <rian.perassoli@gmail.com>
2023-10-05 21:30:04 -0300

    Added global styles to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import styles from "./App.module.css";
+
+import "./globals.css";
+
 function App() {
-  return <p>hello world</p>;
+  return <p className={styles.wrapper}>hello world</p>;
 }
 
 export default App;
